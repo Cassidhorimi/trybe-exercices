ddbeb75f37ba790897a566287aad32aa0c98913a
CassidHorimi <cassid@outlook.com.br>
2021-10-08 17:50:59 -0300

    exercicios 3

--- Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts.js
+++ Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts.js
@@ -1,7 +1,8 @@
 //Para testar alguma linha de codigo descomente ela!
 //1.
-const a = 2;
+const a = 7;
 const b = 3;
+const c = 4;
 
 //console.log(a + b);
 //console.log(a - b);
@@ -18,3 +19,13 @@ else {
   console.log(b);
 } */
 
+//3.
+
+/* if ( a > b && a > c) {
+  console.log(a);
+} else if ( a < b && b > c ) {
+  console.log(b);
+}
+else {
+  console.log(c);
+} */
