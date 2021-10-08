af3f87abd0bf1dcfaa7a5c7fe8c5e0d86e565646
CassidHorimi <cassid@outlook.com.br>
2021-10-08 16:01:16 -0300

    atividade com o operador OR

--- Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts_1.js
+++ Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts_1.js
@@ -1,5 +1,5 @@
 const base = 5;
-var height = 8;
+let height = 8;
 const area = base * height;
 console.log(area)
 const perimeter = base + base + height + height;
--- Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts_3.js
+++ Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts_3.js
@@ -1,25 +1,25 @@
 const currentHour = 15;
-var message = "";
+let message = "";
 
 if (currentHour >= 22 && currentHour <= 24) {
-  var message = " Nao deveriamos comer nada, eh hora de dormir";
+  let message = " Nao deveriamos comer nada, eh hora de dormir";
 }
 else if (currentHour >= 18 && currentHour < 22) {
-  var message = "Rango da noite, vamos jantar :D";
+  let message = "Rango da noite, vamos jantar :D";
 }
 else if (currentHour >= 14 && currentHour < 18) {
-  var message = "Vamos fazer um bolo pro café da tarde?";
+  let message = "Vamos fazer um bolo pro café da tarde?";
 }
 else if (currentHour >= 11 && currentHour < 14) {
-  var message = "Hora do almoço!!!";
+  let message = "Hora do almoço!!!";
 }
 else if (currentHour >= 4 && currentHour < 11 ){
-  var message = "Hmmm, cheiro de café recém passado";
+  let message = "Hmmm, cheiro de café recém passado";
 }
 else if ( currentHour >= 0 && currentHour < 4) {
-  var message = "Voce ja deveria estar dormindo";
+  let message = "Voce ja deveria estar dormindo";
 }
 else {
-  var message = "Horario invalido";
+  let message = "Horario invalido";
 }
 console.log(message);
