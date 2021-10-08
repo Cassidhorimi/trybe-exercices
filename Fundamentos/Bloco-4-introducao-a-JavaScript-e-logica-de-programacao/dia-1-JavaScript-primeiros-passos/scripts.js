1ac66fe5886bd58d97491ac6de2cdcbcb46ee2d7
CassidHorimi <cassid@outlook.com.br>
2021-10-08 15:16:19 -0300

    segunda atividade javascript

--- Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts.js
+++ Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/scripts.js
@@ -3,7 +3,7 @@ const birthCity = "Apucarana";
 let birthYear = 1995;
 birthYear = 2030;
 birthCity = Londrina;
-/* Deu erro porque birthCity é uma constante e não pode ser reatribuida */
+// Deu erro porque birthCity é uma constante e não pode ser reatribuida
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
