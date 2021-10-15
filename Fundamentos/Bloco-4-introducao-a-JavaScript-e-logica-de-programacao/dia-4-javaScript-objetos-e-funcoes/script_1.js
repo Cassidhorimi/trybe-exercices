8e270a74d7d080ba259fc76f692c90d1d051f646
CassidHorimi <cassid@outlook.com.br>
2021-10-15 15:50:36 -0300

    Para fixar -Objetos-3-

--- Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-4-javaScript-objetos-e-funcoes/script_1.js
+++ Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-4-javaScript-objetos-e-funcoes/script_1.js
@@ -4,6 +4,7 @@ let lastName = 'Silva';
 let age = 34;
 let medals = { golden: 2, silver: 3 };
 */
+//
 let athlete = {
   name: 'Marta',
   lastName: 'Silva',
@@ -13,8 +14,13 @@ let athlete = {
     silver: 3,
   }
 }
-console.log('A jogadora ' + athlete.name + ' ' + athlete.lastName + ' tem ' + athlete.age + ' anos de idade.');
+//console.log('A jogadora ' + athlete.name + ' ' + athlete.lastName + ' tem ' + athlete.age + ' anos de idade.');
 
 athlete['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
-
-console.table(athlete);
+/*
+let theBest = '' ;
+for(i = 0;i < athlete.bestInTheWorld.length - 1; i++){
+  theBest += athlete.bestInTheWorld[i] + ', ';
+}
+console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes, em: ' + theBest + athlete.bestInTheWorld[5] + '.');
+*/
