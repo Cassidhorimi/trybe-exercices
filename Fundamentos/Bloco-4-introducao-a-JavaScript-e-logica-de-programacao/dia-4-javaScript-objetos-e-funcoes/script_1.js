c1a0378dc8b9b697810ad12c2ec08edca46fdd32
CassidHorimi <cassid@outlook.com.br>
2021-10-15 15:29:13 -0300

    Para fixar -Objetos-1-

--- Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-4-javaScript-objetos-e-funcoes/script_1.js
+++ Fundamentos/Bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-4-javaScript-objetos-e-funcoes/script_1.js
@@ -4,7 +4,7 @@ let lastName = 'Silva';
 let age = 34;
 let medals = { golden: 2, silver: 3 };
 */
-let atleta = {
+let athlete = {
   name: 'Marta',
   lastName: 'Silva',
   age: 34,
@@ -13,4 +13,4 @@ let atleta = {
     silver: 3,
   }
 }
-console.table(atleta);
+console.log('A jogadora ' + athlete.name + ' ' + athlete.lastName + ' tem ' + athlete.age + ' anos de idade.');
