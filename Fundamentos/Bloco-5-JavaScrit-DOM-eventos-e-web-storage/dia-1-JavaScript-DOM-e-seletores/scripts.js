cdec8d2c8f40f862593d166324c0f273a2ba2d6b
CassidHorimi <cassid@outlook.com.br>
2021-10-19 15:38:30 -0300

    pratica DOM-getElementById

---
+++ Fundamentos/Bloco-5-JavaScrit-DOM-eventos-e-web-storage/dia-1-JavaScript-DOM-e-seletores/scripts.js
@@ -0,0 +1,19 @@
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+paragraph.style.backgroundColor = "gray";
+
+let pageTitle = document.getElementById("page-title");
+pageTitle.innerText = "Pulp Fiction";
+
+let secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.style.backgroundColor = "gray";
+secondParagraph.innerText = "Kill Bill";
+
+let subTitle = document.getElementById("subtitle");
+subTitle.style.color = "blue";
+
+let paragraphs = document.getElementsByClassName("generic-paragraph");
+paragraphs[0].style.color = 'green';
+
+let tags = document.getElementsByTagName('h4');
+tags[0].style.color = 'purple';
