182bb2f4c1fda9b9c5b68e498a53419d7a4f7f6f
CassidHorimi <cassid@outlook.com.br>
2021-10-20 15:53:03 -0300

    fixar parentElement

---
+++ Fundamentos/Bloco-5-JavaScrit-DOM-eventos-e-web-storage/dia-2-JavaScript-trabalhando-com-elementos/scripts_1.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('elementoOndeVoceEsta'));
+console.log(document.getElementById('elementoOndeVoceEsta').parentElement.style.color = 'green');
+console.log(document.getElementById('elementoOndeVoceEsta').firstElementChild.innerText = 'Hello!');
+console.log(document.getElementById('pai').firstElementChild);
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+//console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
+console.log(document.getElementById('pai').lastElementChild.previousElementSibling);
