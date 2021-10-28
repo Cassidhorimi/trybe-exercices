6ed3a331777ab0ff29911eea9883937f14c60dff
CassidHorimi <cassid@outlook.com.br>
2021-10-28 20:01:35 -0300

    pratica forms

---
+++ Fundamentos/Bloco-6-HTML-e-CSS-forms-flexbox-responsivo/dia-1-HTML-e-CSS-forms/script.js
@@ -0,0 +1,13 @@
+const state = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+const stateValue = ['ac', 'al', 'ap', 'am', 'ba', 'ce', 'df', 'es', 'go', 'ma', 'mt', 'ms', 'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs', 'ro', 'rr', 'sc', 'sp', 'se', 'to']
+const select = document.querySelector('#estados-brasil');
+
+function createOptionToSelect() { 
+  for (let  i = 0; i < state.length; i++) {
+    const createOption = document.createElement('option');
+    createOption.innerText = state[i];
+    createOption.value = stateValue[i];
+    select.appendChild(createOption);
+  }
+}
+createOptionToSelect();
