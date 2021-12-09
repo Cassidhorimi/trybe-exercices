4a46ebdea77424c59be8618b2f5486b697579f35
CassidHorimi <cassid@outlook.com.br>
2021-12-09 15:11:51 -0300

    .

--- Fundamentos/Bloco-9-JavaScript-e-Testes-Assincronos/dia-3-Jest-testes-assincronos/getData.js
+++ Fundamentos/Bloco-9-JavaScript-e-Testes-Assincronos/dia-3-Jest-testes-assincronos/getData.js
@@ -5,8 +5,8 @@ const getSuperHero = (url) => {
   const promise = fetch(url)
     .then((response) => response.json())
     .then((hero) => hero.name)
-    // .catch((error) => error);
-  return promise;
+    .catch((error) => error);
+  return console.log(promise);
 };
 
 getSuperHero('https://www.superheroapi.com/api.php/4192484924171229/720'); // Wonder Woman
