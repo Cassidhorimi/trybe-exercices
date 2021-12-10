125206d67f1d2ba1d236ad05f977bb3a8e55f107
CassidHorimi <cassid@outlook.com.br>
2021-12-10 17:58:45 -0300

    exercises 10.2

--- Front-End/Bloco-10-introducao-a-React/dia-2-componentes-react/exercises/src/pokemon.js
+++ Front-End/Bloco-10-introducao-a-React/dia-2-componentes-react/exercises/src/pokemon.js
@@ -8,6 +8,7 @@ class Pokemon extends React.Component {
         <p>{this.props.pokemon.type}</p>
         <p>Avarege weight: {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit}</p>
         <img src={this.props.pokemon.image} alt=""></img>
+        <a href={this.props.pokemon.moreInfo} target="blank" className='link'>More info</a>
       </section>
     )
   }
